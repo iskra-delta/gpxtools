d130b36301765319eaa33b28329e0bd801b50da5
Tomaz Stih <tomaz.stih@wischner.co.uk>
2023-03-05 19:54:45 +0000

    Skeleton of a code generator class finished.

--- lib/cgen/cgen.h
+++ lib/cgen/cgen.h
@@ -18,28 +18,101 @@
 
 namespace cgen
 {
+    // Custom stream buffer that tracks rows.
+    class cstreambuf : public std::streambuf {
+    public:
+        inline cstreambuf(std::streambuf* buf) : buf_(buf) {
+
+        }
+        inline virtual int_type overflow(int_type ch) override {
+            if (ch == '\n') {
+                // Remember start of row position.
+                row_start_ = buf_->pubseekoff(0, std::ios_base::cur);
+                // Add indent.
+                if (indent_)
+                    for(unsigned char i=0;i<indent_;i++)
+                        buf_->sputc(' ');
+            }
+            return buf_->sputc(ch);
+        }
+    
+        // We don't let indent longer than 255...
+        inline void set_indent(unsigned char indent) {
+            indent_=indent;
+        }
+    private:
+        unsigned char indent_ {0}; // Default = no indent.
+        unsigned char tab_size_ {0};
+        std::streambuf* buf_;
+        std::streampos row_start_;
+    };
+
     // Custom stream.
-    class code_stream : public std::basic_ostream<char>
+    class cstream : public std::basic_ostream<char>
     {
     public:
-        inline code_stream(std::streambuf *buf) : std::basic_ostream<char>(buf) {}
-
-        inline code_stream &write(const char *s, std::streamsize n)
-        {
-            return *this;
+        inline cstream(cstreambuf *buf) : std::basic_ostream<char>(buf) {
+            buf_=buf;
         }
-
-        code_stream& set_tab_size(int n) {
-            tab_size_ = n;
+        inline cstream& set_indent(unsigned char indent) {
+            buf_->set_indent(indent);
             return *this;
         }
     private:
-        int tab_size_ = 4; // Default tab size.
+        cstreambuf *buf_;
     };
 
-    code_stream& tab_size(code_stream& os, int n) {
-        os.set_tab_size(n);
-        return os;
+        // File header.
+    class header {
+    public:
+        inline header(
+            std::string filename,
+            std::string desc,
+            std::string license,
+            std::string copyright,
+            std::string author) :
+            filename_(filename),
+            desc_(desc),
+            license_(license),
+            copyright_(copyright),
+            author_(author)
+        {}
+        inline cstream &operator()(cstream &out) const {
+            out << "/*" << std::endl << " * " << filename_ << std::endl
+                << " *" << std::endl << " * " << desc_ << std::endl 
+                << " *" << std::endl 
+                << " * "  << license_ << std::endl
+                << " * " << copyright_ << std::endl
+                << " *" << std::endl
+                << " * " << author_ << std::endl
+                << " *" << std::endl << " */" << std::endl;
+            return out;
+        }
+    private:
+        std::string filename_;
+        std::string desc_;
+        std::string license_;
+        std::string copyright_;
+        std::string author_;
+    };
+
+    class indent {
+    public:
+        inline indent(unsigned char indent) : indent_(indent) {} 
+        inline cstream &operator()(cstream &out) const {
+            out.set_indent(indent_);
+            return out;
+        }
+    private:
+        unsigned char indent_;
+    };
+
+    cstream& operator<<(cstream& out, const indent& indent) {
+        return indent(out);
+    }
+
+    cstream& operator<<(cstream& out, const header& h) {
+        return h(out);
     }
 
 } // namespace cgen
--- src/snatch/snatch.cpp
+++ src/snatch/snatch.cpp
@@ -16,19 +16,28 @@
 #include <cgen/cgen.h>
 #include <snatch/snatch.h>
 
+
+
 int main(int argc, char *argv[]) {
     
     // Parse command line arguments.
     cxx_argp::parser parser;
     struct snatch::snatch_args args(parser);
-    if (parser.parse(argc, argv)) {
-		std::cerr << "parsing OK\n";
-	} else {
-		std::cerr << "there was an error - exiting\n";
+    if (!parser.parse(argc, argv)) {
+		std::cerr << "Invalid command line arguments.\n";
 		return 1;
 	}
 
-	cgen::code_stream cs(std::cout.rdbuf());
-	cs << cgen::tab_size << 8 << "Hello world" << std::endl;
+	cgen::cstreambuf csb(std::cout.rdbuf());
+	cgen::cstream cs(&csb);
 
+	// Code generator.
+	
+	cs 	<< cgen::header("snatch.cpp",
+						"Snatch main file.",
+						"MIT License (see: LICENSE)",
+						"Copyright (c) 2023 Tomaz Stih",
+						"tstih")
+		<< cgen::indent(8) 
+		<< std::endl;
 }
